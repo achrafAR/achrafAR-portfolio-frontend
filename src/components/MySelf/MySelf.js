6747d7a06b0cbfa40018e95b258393719462ac30
achrafAR <achraf.alrachini@gmail.com>
2024-04-15 01:00:31 +0400

    my Slef Page

---
+++ src/components/MySelf/MySelf.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import './mySelf.css';
+import Typewriter from "typewriter-effect";
+
+
+function MySelf() {
+  return (
+    <div className='mySelf-container'>
+      <div className='mySelf-text'>
+        <span>WELCOME TO MY WORLD</span>
+        <h1 className='mySelf-name'>Hi, I’m Achraf Al Rachini</h1>
+        <h1>
+            <Typewriter 
+              options={{
+                strings: ["FullStack Developer!", 
+                          "MernStack Developer!",
+                          "content Writer!"],
+                autoStart: true,
+                loop: true,
+              }}
+              
+            />
+          </h1>
+        <h3>based in UAE.</h3>
+      </div>
+      
+    </div>
+  )
+}
+
+export default MySelf
